3594ed705822dbf3ab9f3c1e379b6a4af6efb761
PupperParty <93052348+PupperParty@users.noreply.github.com>
2022-08-27 10:25:32 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-hedwig = "Hedwig.mp3"
-pp = "PP.mp3"
+hedwig = "Hedwig.mp3";
+pp = "PP.mp3";
+leftWristX = "";
+leftWristY = "";
+rightWristX = "";
+rightWristY = "";
+
 function preload() {
     hedwig = loadSound("Hedwig.mp3");
     pp = loadSound("PP.mp3");
@@ -9,7 +14,29 @@ function setup(){
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 function draw(){
     image(video, 0, 0, 600, 500);
+}
+function play(){
+    song.play();
+}
+function modelLoaded(){
+    console.log('Posenet is Initialized');
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+    }
 }
